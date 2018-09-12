bf7c92b52fa3b87e56badcadde1c8efff414ddc4
Matěj Trakal <mtrakal@gmail.com>
2018-09-12 10:53:18 +0200

    init project

---
+++ app/src/main/java/cz/mtrakal/issue112231683/MainActivity.kt
@@ -0,0 +1,12 @@
+package cz.mtrakal.issue112231683
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
